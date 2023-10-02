171bdedfcc83d826c5ebff6269c3fcf2c17a402b
Guadanhim <111758242+Guadanhim@users.noreply.github.com>
2023-10-02 03:47:09 -0300

    Update ProcessoSeletivo.java
    ```while```

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,13 +1,32 @@
 package candidatura;
-
+import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
-
 	public static void main(String[] args){
-		System.out.println("Processo Seletivo");
-		analisarCandidato(1900.0);
-		analisarCandidato(2000.0);
-		analisarCandidato(2100.0);
-	
+		selecaoCandidatos();
+		
+		
+	}
+	static void selecaoCandidatos() {
+		String [] candidatos = {"Felipe","Marcia","Paulo","Monica","Fabricio","Mirela","Jorge","Luiza","Ana","Maria"};
+		
+		int candidatosSelecionados = 0;
+		int candidatosAtual = 0;
+		double salarioBase = 2000.0;
+		while(candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+			String candidato = candidatos[candidatosAtual];
+			double salarioPretendido = valorPretendido();
+			
+			System.out.println("O candidato" + candidato + "solicitou este valor de salario" + salarioPretendido);
+			if (salarioBase >= salarioPretendido) {
+				System.out.println("O candidato" + candidato + "foi selecionado para a vaga");
+				candidatosSelecionados++;
+			}
+			candidatosAtual++;
+		}
+		
+	}
+    static double valorPretendido() {
+		return ThreadLocalRandom.current().nextDouble(1800,2200);
 	}
 	static void analisarCandidato(double salarioPretendido){
 		double salarioBase = 2000.0;
