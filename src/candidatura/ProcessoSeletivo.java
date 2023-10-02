bac90bb74ffaae57cf499cb5b095e1310a68875c
Guadanhim <111758242+Guadanhim@users.noreply.github.com>
2023-10-02 04:49:27 -0300

    Update ProcessoSeletivo.java

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,8 +1,36 @@
 package candidatura;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
 	public static void main(String[] args){	
-		imprimirSelecionados();
+		String [] candidatos = {"Felipe","Marcia","Paulo","Monica","Fabricio"};
+		for (String candidato: candidatos) {
+			entrandoEmContato(candidato);
+		}
+	}
+	static void entrandoEmContato(String candidato) {
+		int tentativasRealizadas = 1;
+		boolean continuarTentando = true;
+		boolean atendeu = false;
+		do {
+			atendeu = atender();
+			continuarTentando = !atendeu;
+			if(continuarTentando)
+				tentativasRealizadas++;
+			else
+				System.out.println("Contato realizado com sucesso!");
+			
+		}while(continuarTentando && tentativasRealizadas < 3);
+		
+		if (atendeu)
+			System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas + " tentativa ");
+		
+		else 
+			System.out.println("Não conseguimos contato com " + candidato + ", numero maximo tentativas " + tentativasRealizadas + " realizadas ");
+		
+	}
+	static boolean atender() {
+		return new Random().nextInt(3)==1;
 	}
 		
 	static void imprimirSelecionados() {
@@ -11,12 +39,12 @@ public class ProcessoSeletivo {
 		System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
 		
 		for(int indice=0; indice < candidatos.length;indice++) {
-			System.out.println("O candidato de n." + (indice+1) + "é o" + candidatos[indice]);	
+			System.out.println("O candidato de n." + (indice+1) + " é o " + candidatos[indice]);	
 		}
 		System.out.println("Forma abreviada de interação for each");
 		
 		for(String candidato:candidatos) {
-			System.out.println("O candidato selecionado foi" + candidato);
+			System.out.println("O candidato selecionado foi " + candidato);
 		}
 			
 	}	
